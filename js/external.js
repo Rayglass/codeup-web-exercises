8bf79f57e500480d0b502c7417e7bbf3a42a3813
Raymond Glass <raymond.allen.glass@gmail.com>
2023-03-10 14:07:30 -0600

    added inline and external files

---
+++ js/external.js
@@ -0,0 +1,68 @@
+
+"use strict";
+
+//console.log('Hello from External JavaScript!');
+
+//alert('Welcome to my Website!')
+
+//var color = "";
+//while (color === "")
+  //  color = prompt('What is your favorite color?');
+//console.log(color);
+
+// alert('Great, ' + color + ' is my favorite color too!');
+
+/**
+var littleMermaid = "";
+while (littleMermaid === "")
+littleMermaid = prompt('How many days do you want to rent Little Mermaid?');
+console.log(littleMermaid);
+var brotherBear = "";
+while (brotherBear === "")
+    brotherBear = prompt('How many days do you want to rent Brother Bear?');
+console.log(brotherBear);
+var hercules = "";
+while (hercules === "")
+    hercules = prompt('How many days do you want to rent Hercules?');
+console.log(hercules);
+
+var moviePrice = 3
+var littleMermaidTotal = littleMermaid * moviePrice;
+var brotherBearTotal = brotherBear * moviePrice;
+var herculesTotal = hercules * moviePrice;
+var movieTotal = littleMermaidTotal + brotherBearTotal + herculesTotal;
+
+console.log(littleMermaid)
+console.log(brotherBear)
+console.log(hercules)
+console.log(movieTotal)
+
+alert(movieTotal + ' dollars is your total cost.')
+
+ */
+
+var littleMermaid = "";
+while (littleMermaid === "")
+    littleMermaid = prompt('How many days do you want to rent Little Mermaid?');
+console.log(littleMermaid);
+var brotherBear = "";
+while (brotherBear === "")
+    brotherBear = prompt('How many days do you want to rent Brother Bear?');
+console.log(brotherBear);
+var hercules = "";
+while (hercules === "")
+    hercules = prompt('How many days do you want to rent Hercules?');
+console.log(hercules);
+
+var moviePrice = 3
+var littleMermaidTotal = littleMermaid * moviePrice;
+var brotherBearTotal = brotherBear * moviePrice;
+var herculesTotal = hercules * moviePrice;
+var movieTotal = littleMermaidTotal + brotherBearTotal + herculesTotal;
+
+console.log(littleMermaid)
+console.log(brotherBear)
+console.log(hercules)
+console.log(movieTotal)
+
+alert(movieTotal + ' dollars is your total cost.')
