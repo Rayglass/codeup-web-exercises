b456aa3a90b0f44ed549873719c9d9b7bf0369c2
Raymond Glass <raymond.allen.glass@gmail.com>
2023-04-18 15:57:30 -0500

    update

---
+++ js/sandbox.js
@@ -0,0 +1,11 @@
+var array = ['John', 'Paul', 'George', 'Ringo', 'Santana']
+
+console.log(array[2]);
+console.log(array[array.length -2]);
+console.log(array[array.length -3]);
+
+var band = new Object();
+console.log(band);
+
+var band = {};
+console.log(band);
