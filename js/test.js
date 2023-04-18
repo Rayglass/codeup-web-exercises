acf7b7b8bb12919034ecec9a033178868a25e0ae
Raymond Glass <raymond.allen.glass@gmail.com>
2023-04-18 09:37:29 -0500

    file updates

--- js/test.js
+++ js/test.js
@@ -17,9 +17,9 @@
 
 //Examples
 //****Changes color of title in document.
-// const title = document.querySelector('#main-heading');
-// title.style.color = 'red';
-// console.log(title);
+<!--const title = document.querySelector('#main-heading');-->
+<!--title.style.color = 'red';-->
+<!--console.log(title);-->
 
 //***for multiple items you will need to create a for loop in order to process the style command to multiple elements in the code.
 
@@ -54,7 +54,7 @@
 // li.removeAttribute('id')
 //
 // const title = document.querySelector('#main-heading');
-
+//
 // console.log(title.getAttribute('id'));
 //
 // <!--Class List-->
@@ -71,58 +71,3 @@
 
 //Traverse the DOM
 
-// let ul = document.querySelector('ul');
-// console.log(ul);
-// console.log(ul.parentNode);
-// console.log(ul.parentElement);
-// // Double down to get grandparents
-// console.log(ul.parentNode.parentNode);
-// console.log(ul.parentElement.parentElement);
-//
-// console.log(ul.childNodes);
-// console.log(ul.firstChild);
-// console.log(ul.lastChild);
-//
-// ul.childNodes[1].style.backgroundColor = 'blue';
-//
-// console.log(ul.previousSibling);
-// console.log(ul.nextSibling);
-
-//Event Listeners
-
-//element.addListener("click", function);
-
-// const buttonTwo = document.querySelector('.btn-2');
-// function alertBtn() {
-//     alert('I also love JS');
-// }
-//
-// buttonTwo.addEventListener("click", alertBtn);
-//
-// //element.addListener("mouseover", function);
-//
-// const buttonThree = document.querySelector('.btn-3');
-// function changeBgColor() {
-//     buttonThree.style.backgroundColor = 'Blue';
-// }
-//
-// buttonThree.addEventListener("mouseover", changeBgColor);
-//
-const revealBtn = document.querySelector('.reveal-btn');
-const hiddenContent = document.querySelector('.hidden-content');
-
-function revealContent() {
-    if(hiddenContent.classList.contains('reveal-btn')) {
-        hiddenContent.classList.remove('reveal-btn')
-    } else {
-        hiddenContent.classList.add('reveal-btn')
-    }
-}
-
-revealBtn.addEventListener("click", revealContent);
-
-// https://www.youtube.com/watch?v=P7NgFfIaWgU @ 55 minutes
-// Event Probagation
-// 1. Event Capturing
-// 2. EventTargetEvent
-// 3. Bubbling
