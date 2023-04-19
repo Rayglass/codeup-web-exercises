f49fc0844e6d1a9e1535e23dcce8861ca0587bfc
Raymond Glass <raymond.allen.glass@gmail.com>
2023-04-19 10:23:28 -0500

    jquery exercises

--- js/jQueryPractice.js
+++ js/jQueryPractice.js
@@ -1,3 +1,18 @@
-$(function (){
-    alert('The DOM has finished loading!');
-})
+$('li').hover(
+    function() {
+        $(this).css('background-color', 'red');
+    },
+    function() {
+        $(this).css('background-color', '#FFF');
+    }
+);
+
+
+$('h1').click(function() {
+    $(this).css('background-color', 'yellow');
+});
+
+
+$('p').dblclick(function(e) {
+    $(this).css('font-size', '18px');
+});
