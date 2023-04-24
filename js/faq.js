d2d62e93eaa66ecabd9caa1ce970077626850c66
Raymond Glass <raymond.allen.glass@gmail.com>
2023-04-24 14:54:09 -0500

    traversing

--- js/faq.js
+++ js/faq.js
@@ -1,18 +1,44 @@
 (function(){
     "use strict"
 
-    // toggles question answers on and off
+// toggles question answers on and off
     let toggleAll = $('.toggleAll');
     toggleAll.click(function(e){
         $('dd').toggleClass('invisible');
         $(this).toggleClass('btn-on');
     });
 
-
+// highlights individual dt's on click (e-target allows individual elements to be targeted)
 $('dt').click((e) => {
     const target = e.target
     $(target).toggleClass('highlight')
 
     })
+
+})()
+
+// highlights last fact below each h3 header
+let highlightLast = $('.highlight-btn');
+highlightLast.on('click',function(){
+    $(this).siblings('ul').each(function(){
+        $(this).children().last()
+            .css('background-color', 'yellow');
+    });
+    $(this).toggleClass('btn-on');
+
+});
+
+// bolds all facts of parent h3 that is clicked
+$('h3').on('click', function(){
+    let liParent = $(this).next();
+    liParent.children().css('font-weight', 'bold');
+});
+
+
+// changes font color of first fact when sibling fact is clicked
+$('li').on('click', function(){
+    $(this).parent().children().first().css('color', 'blue');
+
+
 
 })()
